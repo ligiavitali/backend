389fb08d120ccb11e0c06932fac189fc29f8d81a
Ligia Vitali <viytaliligia@gmail.com>
2025-05-28 20:52:47 -0300

    adicionado o env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { EventosModule } from 'src/eventos/eventos.module';
 import { LoginModule } from 'src/login/login.module';
 import { NoticiasModule } from 'src/noticias/noticias.module';
@@ -7,16 +8,22 @@ import { ParceirosModule } from 'src/parceiros/parceiros.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123',
-      database: 'projetoextensao',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASS,
+      database: process.env.POSTGRES_DB,
       autoLoadEntities: true,
       synchronize: true,
     }),
+
     LoginModule,
     EventosModule,
     ParceirosModule,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+app.enableCors();
+
+app.useGlobalPipes(
+  new ValidationPipe({
+  transform: true,  
+  whitelist: true,
+  forbidNonWhitelisted: true,
+}));
+
+
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
